17d7ce78d2a0f3851781e4a2c8b85f0d53c7600d
Michael Allen Smith <digitalcolony@gmail.com>
2025-05-15 15:44:50 -0700

    Update dependencies and modify footer content for archival notice

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "INeedCoffee";
+export const SITE_TITLE = "INeedCoffee Archive";
 export const SITE_DESCRIPTION = "By Coffee Fans, For Coffee Fans";
 export const SITE_URL = "https://ineedcoffee.com";
